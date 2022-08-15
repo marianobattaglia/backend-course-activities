76e65f0cdf653e557eda17b0742587dc0cd6cf50
Mariano Battaglia <battaglia1902@gmail.com>
2022-08-15 17:34:24 -0300

    Class 5 - EJS modifications missing files - HandlebarsJS

--- class-5-220806/ejs/index.js
+++ class-5-220806/ejs/index.js
@@ -13,14 +13,14 @@ app.use(express.json());
 
 app.set('view engine', 'ejs');
 
-let productsHC = [
+let productListDB = [
   { id: 1, title: 'iphone se 2022 256gb', price: 520, thumbnail: 'http://localhost:8080/public/iphone-se-2022.jpg' },
   { id: 2, title: 'iphone 13 pro max 512gb', price: 1700, thumbnail: 'http://localhost:8080/public/iphone-13-pro-max.jpeg' },
   { id: 3, title: 'macbook pro m1 8gb 512gb', price: 2000, thumbnail: 'http://localhost:8080/public/macbook-pro-m1.jpeg' },
 ];
 
 app.get('/products', (req, res) => {
-  res.render('pages/products', { title: 'Product List', products: productsHC });
+  res.render('pages/products', { title: 'Product List', products: productListDB });
 });
 
 app.get('/form', (req, res) => {
@@ -29,13 +29,13 @@ app.get('/form', (req, res) => {
 
 app.post('/products', (req, res) => {
   const { body } = req;
-  productsHC.push(body);
-  res.render('pages/products', { title: 'Product List', products: productsHC });
+  productListDB.push(body);
+  res.render('pages/products', { title: 'Product List', products: productListDB });
 });
 
 app.get('/products/:id', (req, res) => {
   let { id } = req.params;
-  const found = productsHC.find((e) => e.id == id);
+  const found = productListDB.find((e) => e.id == id);
   if (found) {
     res.render('pages/product', { title: 'Product detail', product: found });
   } else {
