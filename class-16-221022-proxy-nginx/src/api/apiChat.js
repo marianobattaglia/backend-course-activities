d1eeb0637f2944e664a1ed99f9dd983f24e9cb87
Mariano Martin Battaglia <battaglia1902@gmail.com>
2022-10-22 13:09:22 -0300

    Class 16 challenge - PROXY & NGINX

---
+++ class-16-221022-proxy-nginx/src/api/apiChat.js
@@ -0,0 +1,38 @@
+const fs = require("fs");
+const { normalizeAndDenormalize } = require("../utils/normalizr");
+
+class ApiChat {
+  async writeChatToFile(message) {
+    try {
+      // Normalize data
+      const messagesNormalized = normalizeAndDenormalize("normalize", message);
+
+      await fs.promises.writeFile(
+        "./data/chat.json",
+        JSON.stringify(messagesNormalized)
+      );
+    } catch (err) {
+      console.log("Can't write file, " + err);
+    }
+  }
+
+  async readChatFromFile() {
+    try {
+      // Read normalized message
+      const message = await fs.promises.readFile("./data/chat.json");
+      const messageList = JSON.parse(message);
+
+      // Denormalize message
+      const messagesDenormalized = normalizeAndDenormalize(
+        "denormalize",
+        messageList
+      );
+
+      return messagesDenormalized;
+    } catch (err) {
+      console.log("Can't read file, " + err);
+    }
+  }
+}
+
+module.exports = ApiChat;
