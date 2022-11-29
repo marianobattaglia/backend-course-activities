323392fca66e791b285150d4c842ea4c07713ccf
Mariano Martin Battaglia <battaglia1902@gmail.com>
2022-11-29 20:07:49 -0300

    Class 20 challenge - Arquitectura del servidor

---
+++ class-20-191122-arquitectura-del-servidor/config/config.js
@@ -0,0 +1,19 @@
+import dotenv from "dotenv";
+import path from "path";
+
+dotenv.config({
+  path: path.resolve(process.cwd(), `./config/${process.env.NODE_ENV}.env`),
+});
+
+export default {
+  NODE_ENV: process.env.NODE_ENV || "development",
+  HOST: process.env.HOST || "localhost",
+  PORT: process.env.PORT || 8080,
+  PERS: process.env.PERS || "MEM",
+  db: {
+    name: "ecommerce",
+    collection: "products",
+    connectString: "mongodb://127.0.0.1/",
+    projection: { __v: 0 },
+  },
+};
